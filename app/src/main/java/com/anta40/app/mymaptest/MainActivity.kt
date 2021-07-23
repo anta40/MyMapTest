0a86f25b640fd8ee7d26e511ce1ee0302702c373
Andre Tampubolon <andre@lc.vlsm.org>
2021-07-23 14:30:06 +0700

    first commit

---
+++ app/src/main/java/com/anta40/app/mymaptest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anta40.app.mymaptest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
